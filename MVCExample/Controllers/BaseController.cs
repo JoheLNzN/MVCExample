af157e0c382c27561d73bec1a1a02d074efde36e
Joel C. Naupa Crispín <jnaupac@gmail.com>
2018-02-12 13:22:56 -0500

    Agregando vista index ROL

---
+++ MVCExample/Controllers/BaseController.cs
@@ -0,0 +1,19 @@
+﻿using MVCExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCExample.Controllers
+{
+    public class BaseController : Controller
+    {
+        public DB_ICMEntities bd;
+
+        public BaseController()
+        {
+            bd = new DB_ICMEntities();
+        }
+    }
+}
---
+++ MVCExample/Controllers/RolController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCExample.Controllers
+{
+    public class RolController : BaseController
+    {
+
+        public ActionResult Index()
+        {
+            var roles = (from r in bd.ADM_ROL
+                         select new { r.ROL_ID, r.ROL_DESCRIPTION }).ToList();
+
+            return View(roles);
+        }
+    }
+}
